4ecf7b1c00d821d07e6907cad5d005ed6d09e740
Thi7946 <leonado25460@gmail.com>
2025-01-02 22:07:50 +0700

    Add files via upload

---
+++ hk.loopv,1 basic.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    i=-1
+    count=0
+while 1<= n <=20 and i+1<n:
+    count+=i
+    i+=1
+    print(i*i)
---
+++ hk.pulsminusmultiply v.1basic.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+sum1 = a+b
+sum2 = a-b
+sum3 = a*b
+
+print(sum1)
+print(sum2)
+print(sum3)
